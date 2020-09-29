16f070ac75866d01c92ef909f61827625fb6e6bd
NovaDevs <30295040+NovaDevs@users.noreply.github.com>
2020-09-29 22:06:12 +0200

    Changed name of PixelRGB. It's a test class

---
+++ src/pixelnoid/TestPixelRGB.java
@@ -0,0 +1,44 @@
+package pixelnoid;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+class TestPixelRGB {
+   BufferedImage image;
+   int width;
+   int height;
+   
+   public TestPixelRGB() {
+      try {
+         //String path = new File("src/niveles/pokeball.png").getAbsolutePath();
+         File input = new File("src/niveles/pokeball.png");
+         //System.out.println(path);
+         image = ImageIO.read(input);
+         width = image.getWidth();
+         height = image.getHeight();
+         int[][] arrayNivel = new int[height][width];
+          System.out.println("Width: "+image.getWidth());
+          System.out.println("Height: "+image.getHeight());
+          System.out.println("Array lenght: " + arrayNivel.length);
+
+         for(int i=0; i<height; i++) {
+         
+            for(int j=0; j<width; j++) {
+               Color c = new Color(image.getRGB(j, i));
+               System.out.printf("Pixel[%d][%d], Red: %d, Green: %d, Blue: %d%n",i,j, c.getRed(), c.getGreen(), c.getBlue());
+            }
+         }
+
+      } catch (Exception e) {
+          System.err.println("File not found.");
+      }
+   }
+   
+   static public void main(String args[]) throws Exception {
+      TestPixelRGB obj = new TestPixelRGB();
+      
+   }
+}
